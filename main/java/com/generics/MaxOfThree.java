a2e4e02f67bfdbfe264a596ee29efc54eeff0e85
ShivKokkula <shivbk987@gmail.com>
2021-05-30 12:30:14 +0530

    Refactor - corrected comparison logic in MaxOfThree.java file

--- main/java/com/generics/MaxOfThree.java
+++ main/java/com/generics/MaxOfThree.java
@@ -1,17 +1,17 @@
 package com.generics;
 
-public class MaxOfThree {
+public class MaxOfThree <E extends Comparable<E>>{
 
     public <E extends Comparable<E>> E maxOfThreeGeneric(E element1, E element2, E element3) {
-        if(element1.compareTo(element1) > 0) {
+        if(element1.compareTo(element2) > 0) {
             if (element1.compareTo(element3)> 0)
                 return element1;
             else
                 return element3;
         }
         else {
-            if (element1.compareTo(element3) > 0)
-                return element1;
+            if (element2.compareTo(element3) > 0)
+                return element2;
             else
                 return element3;
         }
